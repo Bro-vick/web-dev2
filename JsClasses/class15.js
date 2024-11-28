e4ce40250738b716911e50ec5c95dff0c6a54f7b
George Victor <georgev421@gmail.com>
2024-11-28 15:57:36 +0100

    Class 15 & task7

---
+++ JsClasses/class15.js
@@ -0,0 +1,52 @@
+// Javascript Built-In Objects
+
+// Date : Used to work with dates and times.
+// methods: getDate(), getMonth(), getFullYear(), getHours(), etc.
+
+let today = new Date();
+console.log(today)
+console.log(today.getDate())
+console.log(today.getDay())
+// Create an array of days use the get day method, to output the current day i.e "Today is Thursday". 
+
+const days = ["Sunday", "Monday", "Tuesday", "Wed", "Thursday", "Fri", "Sat"];
+console.log(`Today is ${days[today.getDay()]}`);
+
+console.log(today.getMonth())
+// Create an array of months use the get month method, to output the current monthh i.e "We are in the month of November". 
+console.log(today.getHours())
+console.log(today.getFullYear())
+console.log(today.toDateString())
+
+let months = []
+
+// Math: Provides inbuilt mathematical functions and constants.
+// (Properties: PI, E, etc), (Methods: abs(), ceil(), floor(), max(), random(), etc)
+
+console.log(Math.PI);
+console.log(Math.E);
+
+console.log(Math.abs(-5));
+console.log(Math.sqrt(16));
+console.log(Math.random());
+// Multiply by 10 for random numbers btw 1 and 10
+console.log("Random number")
+console.log(Math.random() * 10);
+let randomNumber = Math.round(Math.random() * 10)
+console.log(randomNumber);
+
+console.log(Math.floor(3.8));
+console.log(Math.ceil(3.1));
+
+// Create a function that generates a number of random integers n, random integers btw 1 and 100, depending on the input. 
+// i.e randInt(5) -- 13, 2, 78, 21, 84
+function randInt(n){
+    let output = ""
+    for (let i = 0; i < n; i++){
+        let randInt = Math.round(Math.random() * 10);
+        (i+1 == n) ? output += ` ${randInt}.` : output += ` ${randInt}, `;
+    }
+    console.log()
+};
+console.log("Random Numbers:")
+randInt(5);
